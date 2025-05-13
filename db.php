e7928808f08e0d077468679f301ce8dc60a6bc31
unknown <houtmeng068@gmail.com>
2025-05-13 23:43:53 +0700

    vercel

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
 
-$servername = "localhost"; 
-$username = "root";
-$password = ""; 
-$database = "studentdb";
+$servername = "sql206.infinityfree.com"; 
+$username = "if0_38973093";
+$password = "8psDN2LGSLdXy7 "; 
+$database = "if0_38973093_studentdb";
+$port = 3306;
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database, port: $port);
 
 
 if ($conn->connect_error) {
